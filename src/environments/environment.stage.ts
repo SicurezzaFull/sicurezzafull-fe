e9179f5256acb69e608a05b4595c4a5c7d2810f4
Avila George <george.avila@external.bper.it>
2024-09-19 12:45:04 +0200

    change endpoint

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    endpoint: 'https://sicurezzafull-be.onrender.com/',
+    endpoint: 'https://prod-sicurezzafull-be.onrender.com/',
     googleMapsApiKey: 'AIzaSyA1JYdBfuWQxh1CIiQLpiai0A-EPxYtogIs',
     spacesEndpoint: 'fra1.digitaloceanspaces.com',
     accessKeyId: 'DO008T6X3J72C7PBM4VN',
